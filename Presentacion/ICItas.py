d5a767340b97ebbefe3fd3c8bce2a00c31bb0382
luideoz <Luis.Benito@alu.uclm.es>
2024-01-31 19:07:36 +0100

    reseteo interfaz

--- Presentacion/ICItas.py
+++ Presentacion/ICItas.py
@@ -3,22 +3,5 @@ from Settings.colors import *
 
 class ICitas:
     def __init__(self):
-        
-        #window configuration
         self.window = Tk()
-        self.window.title("Generacion de citas")
-        self.window.geometry("750x750")
-        self.window.resizable(False, False)
-        self.window.config(bg=color_bg)
-        
-        #empezamos con la parte de eleccion de autor
-        #diposicion de la ventana: dividida en dos, en la izquierda tendremos la parte de seleccion de autor si esta creado y en la derecha tendremos la parte de creacion de autor. arriba en el centro texto explicativo y abajo ederecha dos botones:avanzar y retroceder
-        #parte izquierda
-        self.leftFrame = Frame(self.window, bg="white")
-        self.leftFrame.pack(side=LEFT, padx=20, pady=20)
-        #parte derecha
-        self.rightFrame = Frame(self.window, bg="white")
-        self.rightFrame.pack(side=RIGHT, padx=20, pady=20)
-        
-        
         self.window.mainloop()
--- Presentacion/IMenu.py
+++ Presentacion/IMenu.py
@@ -1,33 +1,12 @@
 from tkinter import *
 from Settings.colors import *
 from Settings.images import *
-from Presentacion.ICitas import ICitas
+from Presentacion.ICItas import ICitas
 
 class IMenu:
     
     def __init__(self):
         #window configuration
         self.window = Tk()
-        self.window.title("ApaCreator")
-        self.window.geometry("400x400")
-        self.window.resizable(False, False)
-        #definimos una imagen para ponerla como icono de la aplicacion
-        icon = PhotoImage(file=imgIcon)
-        self.window.config(bg=color_bg)
-        self.window.iconphoto(False, icon)
-        self.title = Label(self.window, text="ApaCreator", bg=color_bg, fg=color_fg, font=("Garamond", 30))
-        self.title.pack(pady=20)
-        
-        #button configuration
-        #los botones centrados al margen izquierdo
-        #definimos las imagenes para los botones
-        kitty1 = PhotoImage(file=imgButton1)
-        kitty2 = PhotoImage(file=imgButton2)
-        
-        self.citasButton = Button(self.window, text=" Generar Citas", bg=color_bg_button, fg=color_fg_button, width=300, height=70, font=("Garamond", 15), activebackground=color_bg_button, activeforeground=color_fg_button, image=kitty1, compound='left')
-        self.citasButton.pack(pady=20)
-        self.resetButton = Button(self.window, text=" Resetear Datos", bg=color_bg_button, fg=color_fg_button, width=300, height=70, font=("Garamond", 15), activebackground=color_bg_button, activeforeground=color_fg_button, image=kitty2, compound='left')
-        self.resetButton.pack(pady=20)
-        
         self.window.mainloop()
         
